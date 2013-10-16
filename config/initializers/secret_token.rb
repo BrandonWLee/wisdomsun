01e8a0cfc9e17e6772372d3e31b8de800caae4e0
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-10-16 01:29:42 -0700

    Add welcome index for homepage.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wisdomsun::Application.config.secret_token = '9ec927dd4c6e7d5e4e6c55b1a629cf707d394008ca1a6b274827e6588484c3bb3b76688407e36ff04e9bef89b71a3e35aeb9dca490a12733403ece57408bd4ab'
