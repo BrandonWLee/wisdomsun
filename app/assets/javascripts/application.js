4fc6502553a8eec88b010af4df6b8409d16ef160
Jeff Zhan <saasbook@saasbook.(none)>
2013-10-26 01:07:47 -0700

    Nav Menu functionality working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  var path = window.location.pathname.split('/')[1];
+  var menu_item;
+  switch(path){
+    case 'about':
+      menu_item = '#menu-about';
+      break;
+    case 'books':
+      menu_item = '#menu-books';
+      break;
+    case 'downloads':
+      menu_item = '#menu-downloads';
+      break;
+    case 'bulletin':
+      menu_item = '#menu-bulletin';
+      break;
+    case 'contact':
+      menu_item = '#menu-contact';
+      break;
+    default:
+      menu_item = '#menu-home';
+  }
+  $(menu_item).addClass('active');
+});
