cb3d6eee46014358dfcd6ed6b211ba0c3ebc4db8
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-10-26 03:52:59 -0700

    Change description attribute for Event model to type :text instead of :string. This is because :string has an implicit limit of 255 characters, but description can be pretty long, right?

--- db/migrate/20131024051732_create_events.rb
+++ db/migrate/20131024051732_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :name
       t.string :date
-      t.string :description
+      t.text :description
       t.string :location
       t.string :time
       t.timestamps
