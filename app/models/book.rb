fb7f3d88276d529db15a1a462c5b693c81dc7a53
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-10-27 02:39:02 -0700

    Delete unwanted migration that adds picture to book.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ require "paperclip/storage/ftp"
 class Book < ActiveRecord::Base
   attr_accessible :author, :description, :purchase_url, :title
   attr_accessible :picture, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at
-  has_attached_file :picture,
+  has_attached_file :picture, 
     :storage => :ftp,
     :path => "/wisdomsun.org/dev/:filename",
     :url => "http://wisdomsun.org/dev/:filename",
