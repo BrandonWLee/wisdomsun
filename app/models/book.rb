000c8ec9d8ca8f445f400e1b90b370cce6a6908e
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-10-27 23:41:14 -0700

    Add coverage to git

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,15 +3,6 @@ class Book < ActiveRecord::Base
   attr_accessible :author, :description, :purchase_url, :title
   attr_accessible :picture, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at
   has_attached_file :picture, 
-    :storage => :ftp,
     :path => "/wisdomsun.org/dev/:filename",
-    :url => "http://wisdomsun.org/dev/:filename",
-    :ftp_servers => [
-      {
-        :host   => "web1018.hostexcellence.com",
-        :user   => "***REMOVED***",
-        :password => "***REMOVED***",
-        :passive => true
-      }
-    ]
+    :url => "http://wisdomsun.org/dev/:filename"
 end
