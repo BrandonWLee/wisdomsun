d1ed8ca7ebaa2a317897eaa05f100dfa6cee4155
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-10-29 22:48:49 -0700

    Play with precompile in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,7 @@ Wisdomsun::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += %w( *.css *.js *.scss )
-
+  config.assets.precompile += %w(frontend.css frontend.js active_admin.css active_admin.js application.css application.js welcome.css.scss)
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
