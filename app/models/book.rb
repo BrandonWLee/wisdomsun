ead35d167f763ec6fc53448f6bfc880a1cc6bb93
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-11-03 04:30:07 -0800

    Allow upload of book photo. Change admin display.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,18 @@
 require "paperclip/storage/ftp"
 class Book < ActiveRecord::Base
   attr_accessible :author, :description, :purchase_url, :title
-  attr_accessible :picture, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at
+  attr_accessible  :picture, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at
   has_attached_file :picture,
+    :storage => :ftp,
     :path => "/wisdomsun.org/dev/:filename",
-    :url => "http://wisdomsun.org/dev/:filename"
+    :url => "http://wisdomsun.org/dev/:filename",
+    :ftp_servers => [ {
+      :host => ENV['FTP_HOST'],
+      :user => ENV['FTP_USERNAME'],
+      :password => ENV['FTP_PASSWORD'],
+      :passive => true
+      } ]
+  
+  validates_attachment :picture, 
+    :content_type => { :content_type => "image/jpg" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(:version => 20131102194918) do
     t.string   "title"
     t.string   "author"
     t.text     "description"
-    t.string   "picture"
     t.string   "purchase_url"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
