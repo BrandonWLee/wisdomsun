47fc3b830082d7b649f3b60af90f078c5df7b4ab
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-11-05 22:14:51 -0800

    Organize filepath structure on ftp server for uploads in book, album, and photos. Also, add photo attachment to book seed.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,12 @@
 require "paperclip/storage/ftp"
 class Book < ActiveRecord::Base
   attr_accessible :author, :description, :purchase_url, :title
-  attr_accessible  :picture, :picture_file_name, :picture_content_type, :picture_file_size, :picture_updated_at
+  attr_accessible  :picture, :picture_file_name
+  attr_accessible :picture_content_type, :picture_file_size, :picture_updated_at
   has_attached_file :picture,
     :storage => :ftp,
-    :path => "/wisdomsun.org/dev/:filename",
-    :url => "http://wisdomsun.org/dev/:filename",
+    :path => "/wisdomsun.org/dev/:class/:attachment/:id/:style/:filename",
+    :url => "http://wisdomsun.org/dev/:class/:attachment/:id/:style/:filename",
     :ftp_servers => [ {
       :host => ENV['FTP_HOST'],
       :user => ENV['FTP_USERNAME'],
