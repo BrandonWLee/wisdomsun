af03a27a82d4bf4add4f57566f04e49547710824
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-11-10 23:17:56 -0800

    Write album_admin rspec test.

--- spec/admin/category_contr_spec.rb
+++ spec/admin/category_contr_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require 'factory_girl'
-
 describe Admin::CategoriesController do
 	
 	it "should redirect to '/' after post destroy" do
--- spec/controllers/books_index_and_show_spec.rb
+++ spec/controllers/books_index_and_show_spec.rb
@@ -4,15 +4,16 @@ describe BooksController do
 	describe 'the show controller method' do
 		before :each do
 			@book = mock('book')
+      Book.stub(:exists?).and_return(true)
 		end
 		it 'should render the show template' do
 			Book.stub(:find).and_return(@book)
-			post :show, {:id => 1}
+			get :show, {:id => 1}
 			response.should render_template('books/show')
 		end
 		it 'should pass the correct id param to the find model' do
 			Book.should_receive(:find).with(1.to_s).and_return(@book)
-			post :show, {:id => 1}
+			get :show, {:id => 1}
 			assigns(:book).should == @book
 		end
 	end
@@ -23,12 +24,12 @@ describe BooksController do
 		end
 		it 'should render the index template' do
 			Book.stub(:all).and_return(@books)
-			post :index
+			get :index
 			response.should render_template('index')
 		end
 		it 'should pass the a collection of all books to the view' do
 			Book.should_receive(:all).and_return(@books)
-			post :index
+			get :index
 			assigns(:books). should == @books
 		end
 	end
