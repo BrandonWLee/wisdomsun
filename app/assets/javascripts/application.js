b86b521fac639906b0545a584239145df8a925d1
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-11-12 06:35:01 -0800

    Fix fancybox js issue, hopefully. Explicitly specify js files in application.js rather than require_tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,20 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-//= require layout
-//= require jquery.flexslider-min
-//= require redactor-rails
-//= require fancybox/jquery.fancybox-1.3.4.js
+//= require jquery-ui
+//= require_directory ./redactor-rails
+//= require active_admin.js
+//= require books.js
+//= require category.js
+//= require events.js
+//= require layout.js
+//= require other_pages.js
+//= require users.js
+//= require welcome.js
+//= require_directory ./woothemes-FlexSlider
+//= require jquery.flexslider-min.js
+//= require jquery-1.4.3.min.js
+//= require_directory ./fancybox
+//= require jquery.fancybox-1.3.4.js
+//= require photos.js
+////= require_tree .
