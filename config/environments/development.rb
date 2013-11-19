d875466409d55828360367d26a8acb56029662ac
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-11-19 05:22:27 -0800

    Send emails for forgotten password (need actual smtp service like gmail). Dev debug mode to off.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Wisdomsun::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -33,10 +33,22 @@ Wisdomsun::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Devise default url
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   :address => ENV["SMTP_ADDRESS"], 
+   :port => 587,
+   :domain => "gmail.com",
+   :user_name => ENV["SMTP_USERNAME"],
+   :password  => ENV["SMTP_PASSWORD"],
+   :authentication  => :plain,
+   :enable_starttls_auto => true
+   }
+
 
   # Paperclip using ImageMagick
   Paperclip.options[:command_path] = "/usr/bin/"
