f70c2dd7045e717fcf13409943211d4cd474d761
SL <saasbook@saasbook.(none)>
2013-11-23 11:56:11 -0800

    seed for Articles, Songs and More

--- db/seeds.rb
+++ db/seeds.rb
@@ -180,6 +180,32 @@ d_p1 = OtherPage.create!({:category_id => 3, :title => "Free Downloads", :refact
 </span></h4>
 <p> </p>
 "})
-d_p2 = OtherPage.create!({:category_id => 3, :title => "Downloads Pg2", :refactored_title => "d_pg2", :body => "Random filler text for now."})
+
+d_p2 = OtherPage.create!({:category_id => 3, :title => "Articles, Songs and More", :refactored_title => "articles,_songs_and_more", :body => "<p><span style=\"color: #001d2c; font-size: 13px; line-height: 24px;\">Click on the links below to hear songs in English and read Tibetan texts:</span></p>
+<p> </p>
+<h4 style=\"font-size: 1.3077em; line-height: 1.3em; font-weight: normal; color: #971500; font-style: italic; margin-top: 0.909em; margin-bottom: 0.909em;\">Songs</h4>
+<ul style=\"margin-top: 1em; margin-right: 0px; margin-bottom: 1em; margin-left: 0px; padding-left: 2em; list-style-type: disc;\">
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/audio/Seven%20Ways%20Things%20Shine%20Inside%20and%20Out.mp3\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Seven Ways Things Shine Inside And Out</a> (mp3)</li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/audio/05%20Eighteen%20Kinds%20Of%20Yogic%20Joy.mp3\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Eighteen Kinds Of Yogic Joy</a> (mp3)</li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/audio/08%20Auspiciousness%20That%20Lights%20Up%20The%20Universe.mp3\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Auspiciousness That Lights Up The Universe</a> (mp3)</li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/audio/Cornwall%20Offering%20Song.mp3\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Cornwall Offering Song</a> (mp3)</li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/pdf/cornwall-offering-song.pdf\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Cornwall Offering Song</a> (pdf)</li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/audio/A%20Prayer%20that%20these%20True%20Words%20be%20Swiftly%20Fulfilled.mp3\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">A Prayer that these True Words be Swiftly Fulfilled</a> (mp3)</li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/pdf/a-prayer-that-these-true-words-be-swiftly-fulfilled-te.pdf\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">A Prayer that these True Words be Swiftly Fulfilled</a> (pdf)</li>
+</ul>
+<h4 style=\"font-size: 1.3077em; line-height: 1.3em; font-weight: normal; color: #971500; font-style: italic; margin-top: 0.909em; margin-bottom: 0.909em;\"><span style=\"color: #333333; font-family: Tahoma, Helvetica, Arial, sans-serif; font-size: 12px; line-height: 15px; font-style: normal;\"> 
+<ul style=\"margin-top: 1em; margin-right: 0px; margin-bottom: 1em; margin-left: 0px; padding-left: 2em; list-style-type: disc;\">
+</ul>
+</span></h4>
+<h4 style=\"font-size: 1.3077em; line-height: 1.3em; font-weight: normal; color: #971500; font-style: italic; margin-top: 0.909em; margin-bottom: 0.909em;\">Articles</h4>
+<ul style=\"margin-top: 1em; margin-right: 0px; margin-bottom: 1em; margin-left: 0px; padding-left: 2em; list-style-type: disc;\">
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/pdf/stages-of-view-at-the-heart-of-definitive-meaning-root-verses-english.pdf\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Stages of View at the Heart of Definitive Meaning - Root Verses - English</a></li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/pdf/stages-of-view-at-the-heart-of-definitive-meaning-root-verses-tibetan.pdf\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Stages of View at the Heart of Definitive Meaning - Root Verses - Tibetan</a></li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/pdf/bd-bookreview-by-ari-goldfield-life-of-milarepa-book.pdf\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Buddhadharma: <em>The Life of Milarepa</em> Reviewed by Ari Goldfield</a></li>
+<li style=\"padding: 0px; margin: 8px 0;\"><a href=\"http://www.wisdomsun.org/images/pdf/bd-love-me-hate-me-by-rose-taylor.pdf\" target=\"_blank\" style=\"color: #d55e34; text-decoration: none;\">Buddhadharma: <em>Love Me, Hate Me</em> by Rose Taylor</a></li>
+</ul>
+<p> </p>
+"})
+
 d_p3 = OtherPage.create!({:category_id => 3, :title => "Downloads Pg3", :refactored_title => "d_pg3", :body => "Random filler text for now."})
 
