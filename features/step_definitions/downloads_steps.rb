82686b18310430c9b46d234eda5c94288504aa97
SL <saasbook@saasbook.(none)>
2013-11-23 23:27:18 -0800

    New step definitions and validation reflection

---
+++ features/step_definitions/downloads_steps.rb
@@ -0,0 +1,37 @@
+#encoding: utf-8
+
+When /the downloads pages have been seeded/ do
+	load "#{Rails.root}/db/seeds.rb"
+end
+
+Then /I should be able to click Guided Earth Grounding Meditation/ do
+	page.should have_link('Guided Earth Grounding Meditation - Ari Goldfield', href: "http://www.wisdomsun.org/images/audio/20110710-Ari_Goldfield-IMC-guided_earth_grounding_meditation.mp3")
+end
+
+Then /I should be able to click The Nature of Mind/ do
+	page.should have_link('The Nature of Mind - Ari Goldfield', href: "http://www.wisdomsun.org/images/audio/20110710-Ari_Goldfield-IMC-the_true_nature_of_mind.mp3")
+end
+
+Then /I should be able to click Pure Perception and Natural Confidence/ do
+	page.should have_link('Pure Perception and Natural Confidence—Ari Goldfield', href: "http://www.wisdomsun.org/images/audio/StarsOfWisdom1-PurePerceptionAndNaturalConfidence--AriGoldfield.mp3")
+end
+
+Then /I should be able to click Selflessness View and Meditation/ do
+	page.should have_link('Selflessness View and Meditation—Rose Taylor', href: "http://www.wisdomsun.org/images/audio/StarsOfWisdom2-SelflessnessViewAndMeditation--RoseTaylor.mp3")
+end
+
+Then /I should be able to click Seven Ways Things Shine Inside And Out/ do
+	page.should have_link('Seven Ways Things Shine Inside And Out', href: "http://www.wisdomsun.org/images/audio/Seven%20Ways%20Things%20Shine%20Inside%20and%20Out.mp3")
+end
+
+Then /I should be able to click Eighteen Kinds Of Yogic Joy/ do
+	page.should have_link('Eighteen Kinds Of Yogic Joy', href: "http://www.wisdomsun.org/images/audio/05%20Eighteen%20Kinds%20Of%20Yogic%20Joy.mp3")
+end
+
+Then /I should be able to click Stages of View at the Heart of Definitive Meaning/ do
+	page.should have_link('Stages of View at the Heart of Definitive Meaning - Root Verses - English', href: "http://www.wisdomsun.org/images/pdf/stages-of-view-at-the-heart-of-definitive-meaning-root-verses-english.pdf")
+end
+
+Then /I should be able to click Buddhadharma/ do
+	page.should have_link('Buddhadharma: Love Me, Hate Me by Rose Taylor', href: "http://www.wisdomsun.org/images/pdf/bd-love-me-hate-me-by-rose-taylor.pdf")
+end
