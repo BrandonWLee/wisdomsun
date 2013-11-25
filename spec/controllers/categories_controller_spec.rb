2e5308bd29f6197d2d48b1862d61b2461512f5d1
SL <saasbook@saasbook.(none)>
2013-11-24 19:55:51 -0800

    fixed bugs in the categories_controller_spec.rb rspec so it should pass now

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,25 +3,26 @@ require 'spec_helper'
 describe CategoriesController do
   describe 'the index controller method' do
     before :each do
-      @category = Category.create(:name => "meep", :refactored_name => "meep")
-      @category2 = Category.create(:name => "meep2", :refactored_name => "meep2")
+      @category = Category.create(:name => "meep", :refactored_name => "meep", :position => 98)
+      @category2 = Category.create(:name => "meep2", :refactored_name => "meep2", :position => 52)
     end
     it 'renders index correctly' do
       get :index
+      @category.name.should == "meep"
       response.should render_template('index')
     end
     it 'the show controller method' do
-      get :show, :id => 1
+      get :show, :id => @category.id
       # @photo1.destroy
       response.should render_template('show')
     end
     it 'can access pages thru getpage' do
-      @page = OtherPage.create(:title => "bloop", :refactored_title => "bloop1", :category_id => 1)
+      @page = OtherPage.create(:title => "bloop", :refactored_title => "bloop1", :category_id => @category.id)
       get :getpage, :category_name => "meep", :page_name => "bloop"
     end
     after :each do
       @category.destroy
-      @category1.destroy
+      @category2.destroy
     end
   end
 end
