a57360302d1b008ccd4a82a5a8655398ff898afe
Kevin Di Pasupil <saasbook@saasbook.(none)>
2013-11-24 22:13:28 -0800

    Fix to from bug (still needs to be refined tho)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def contact_email(name, email, subject, message)
-    mail(to: email, 
-         subject: subject,
+  def contact_email(name, from, to, subject, message)
+    mail(from: from,
+         to: to, 
+         subject: "'#{subject}' from #{name}",
          body: message,
          content_type: "text/html")
   end
