44e3efc1dce299f6588d16c2497aa1b3b50c826b
Brandon Lee <brandonlee.0727@berkeley.edu>
2013-12-15 22:59:10 -0800

    Seems common compatible jquery version for fancybox/slider/forem/redactor is 1.7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,9 @@
 //= require jquery-ui
 //= require_directory ./redactor-rails
 //= require active_admin.js
+//= require jquery-1.7.min.js
 //= require_directory ./woothemes-FlexSlider
 //= require jquery.flexslider-min.js
-//= require jquery-1.10.2.min.js
 //= require forem
 //= require_directory ./fancybox
 //= require jquery.fancybox-1.3.4.js
@@ -29,3 +29,4 @@
 //= require other_pages.js
 //= require users.js
 //= require welcome.js
+////= require_tree .
